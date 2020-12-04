3549eb4df1a2ddc126e73c12c4066942b9446ff6
Liza_Voronkova <liza.voronkova.01@mail.ru>
2020-12-04 19:15:23 +0400

    Lab 4 is ready

--- WindowsFormsPlaneUsl/FormAirport.cs
+++ WindowsFormsPlaneUsl/FormAirport.cs
@@ -12,51 +12,63 @@ namespace WindowsFormsPlaneUsl
 {
     public partial class FormAirport : Form
     {
-        private readonly Airport<Warplane,DrawingBombsContainer> airport;
+        private readonly AirportCollection airportCollection;
+
+        private Stack<IFlyingTransport> stack = new Stack<IFlyingTransport>();
 
         public FormAirport()
         {
             InitializeComponent();
-            airport = new Airport<Warplane,DrawingBombsContainer>(pictureBoxAirport.Width, pictureBoxAirport.Height);
+            airportCollection = new AirportCollection(pictureBoxAirport.Width, pictureBoxAirport.Height);
             Draw();
+        }
+
+        private void ReloadLevels()
+        {
+            int index = listBoxAiports.SelectedIndex;
+            listBoxAiports.Items.Clear();
+            for (int i = 0; i < airportCollection.Keys.Count; i++)
+            {
+                listBoxAiports.Items.Add(airportCollection.Keys[i]);
+            }
+            if (listBoxAiports.Items.Count > 0 && (index == -1 || index >= listBoxAiports.Items.Count))
+            {
+                listBoxAiports.SelectedIndex = 0;
+            }
+            else if (listBoxAiports.Items.Count > 0 && index > -1 && index < listBoxAiports.Items.Count)
+            {
+                listBoxAiports.SelectedIndex = index;
+            }
         }
 
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(pictureBoxAirport.Width, pictureBoxAirport.Height);
-            Graphics gr = Graphics.FromImage(bmp);
-            airport.Draw(gr);
-            pictureBoxAirport.Image = bmp;
+            if (listBoxAiports.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxAirport.Width, pictureBoxAirport.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                if (listBoxAiports.SelectedIndex > -1)
+                {
+                    airportCollection[listBoxAiports.SelectedItem.ToString()].Draw(gr);
+                }
+                else
+                {
+                    gr.FillRectangle(new SolidBrush(System.Drawing.Color.Transparent), 0, 0, pictureBoxAirport.Width, pictureBoxAirport.Height);
+                }
+                pictureBoxAirport.Image = bmp;
+            }
+
         }
 
         private void buttonSetWarplane_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxAiports.SelectedIndex > -1)
             {
-                var warplane = new Warplane(100, 100, dialog.Color);
-                if (airport + warplane)
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    Draw();
-                }
-                else
-                {
-                    MessageBox.Show("Парковка переполнена");
-                }
-            }
-        }
-
-        private void buttonSetFighter_Click(object sender, EventArgs e)
-        {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == DialogResult.OK)
-                {
-                    var fighter = new Fighter(100, 100, dialog.Color, dialogDop.Color, true, true,6,BombsForm.TRIANGLE);
-
-                    if (airport + fighter)
+                    var warplane = new Warplane(100, 100, dialog.Color);
+                    if (airportCollection[listBoxAiports.SelectedItem.ToString()] + warplane)
                     {
                         Draw();
                     }
@@ -68,18 +80,81 @@ namespace WindowsFormsPlaneUsl
             }
         }
 
+        private void buttonSetFighter_Click(object sender, EventArgs e)
+        {
+            if (listBoxAiports.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var fighter = new Fighter(100, 100, dialog.Color, dialogDop.Color, true, true, 6, BombsForm.TRIANGLE);
+
+                        if (airportCollection[listBoxAiports.SelectedItem.ToString()] + fighter)
+                        {
+                            Draw();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Парковка переполнена");
+                        }
+                    }
+                }
+            }
+        }
+
         private void buttonTakePlane_Click(object sender, EventArgs e)
         {
-            if (maskedTextBox.Text != "")
+            if (listBoxAiports.SelectedIndex > -1 && maskedTextBox.Text != "")
             {
-                var warplane = airport - Convert.ToInt32(maskedTextBox.Text);
+                var warplane = airportCollection[listBoxAiports.SelectedItem.ToString()] - Convert.ToInt32(maskedTextBox.Text);
                 if (warplane != null)
                 {
-                    FormFighter form = new FormFighter();
-                    form.SetWarplane(warplane);
-                    form.ShowDialog();
+                    stack.Push(warplane);
                 }
                 Draw();
+            }
+        }
+
+        private void ButtonDelAirport_Click(object sender, EventArgs e)
+        {
+            if (listBoxAiports.SelectedIndex > -1)
+            {
+                if (MessageBox.Show($"Удалить парковку {listBoxAiports.SelectedItem.ToString()}?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    airportCollection.DelAirport(listBoxAiports.SelectedItem.ToString());
+                    ReloadLevels();
+                }
+            }
+        }
+
+        private void ListBoxParkings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void ButtonAddAirport_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNewLevelName.Text))
+            {
+                MessageBox.Show("Введите название парковки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            airportCollection.AddAirport(textBoxNewLevelName.Text);
+            ReloadLevels();
+            listBoxAiports.SetSelected(listBoxAiports.Items.Count - 1, true);
+            Draw();
+        }
+
+        private void ButtonFly_Click(object sender, EventArgs e)
+        {
+            if (stack.Count > 0)
+            {
+                FormFighter form = new FormFighter();
+                form.SetWarplane(stack.Pop());
+                form.ShowDialog();
             }
         }
     }
--- WindowsFormsPlaneUsl/IFlyingTransport.cs
+++ WindowsFormsPlaneUsl/IFlyingTransport.cs
@@ -10,7 +10,9 @@ namespace WindowsFormsPlaneUsl
     public interface IFlyingTransport
     {
         void SetPosition(int x, int y, int width, int height);
+
         void MoveTransport(Direction direction);
+
         void DrawTransport(Graphics g);
     }
 }
